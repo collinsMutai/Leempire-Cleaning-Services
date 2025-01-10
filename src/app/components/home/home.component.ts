a2bb228a6506e675d6943d04eb47f7bf69c4808c
Collins Mutai <collinsfrontend@gmail.com>
2025-01-10 10:49:19 +0300

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ImageSliderComponent } from '../image-slider/image-slider.component';
 import { ServicesCarouselComponent } from '../services-carousel/services-carousel.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ImageSliderComponent, ServicesCarouselComponent],
+  imports: [ImageSliderComponent,   AboutComponent, ServicesCarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
